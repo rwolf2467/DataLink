b3b53c2bb47aae4291a637d7fa3fd86a67e03af3
Red_wolf2467 <redwolf2467@pyropixle.com>
2024-02-18 17:21:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import flask
 
 app = flask.Flask(__name__, template_folder="web", static_folder="static")
 
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"txt", "pdf", "zip", "mp4", "mp3", "jpg", "jpeg", "gif", "png", "docx", "doc", "ppt"}
